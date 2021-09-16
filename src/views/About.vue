519348503fd671c40a986b76958adb58eb6a90eb
pywang <1394044539@qq.com>
2021-09-16 17:35:19 +0800

    引入antd

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-input v-model:value="value" placeholder="Basic usage"></a-input>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,28 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import { HomeOutlined } from '@ant-design/icons-vue'
+import {ref} from 'vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomeOutlined
+  },
+  setup(){
+    const value=ref(3)
+
+    return{
+      value:value
+    }
   }
 }
 </script>
